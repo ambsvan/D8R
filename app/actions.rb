f6b0eef8d13631467da821137e7f99abcaccdbee
ambsvan <amberslouise@gmail.com>
2014-06-25 21:57:53 +0000

    signup and login forms done

--- app/actions.rb
+++ app/actions.rb
@@ -1,9 +1,3 @@
-
-get '/' do
-  erb :index
-end
-#loads homepage
-
 helpers do
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,13 +5,19 @@ helpers do
 end
 #starts session for current user
 
+
+get '/' do
+  erb :index
+end
+#loads homepage
+
 post '/' do
   @user = User.new(
     username: params[:username],
     password: params[:password],
     img:  params[:img],
     age: params[:age],
-    gender: params[:gender]
+    gender: params[:gender],
     user_id: session[:user_id].to_i)
   if @user.save
     session[:user_id] = @users.id
