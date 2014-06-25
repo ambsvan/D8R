ea198b69778d7ae5ab0a9738dcb504f62759456d
ambsvan <amberslouise@gmail.com>
2014-06-25 20:17:25 +0000

    added new user in action.rb

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,24 @@
 get '/' do
   erb :index
 end
+
+#loads homepage
+
+post '/new' do
+  @user = User.new(
+    username: params[:username],
+    password: params[:password],
+    img:  params[:img],
+    age: params[:age],
+    gender: params[:gender]
+    user_id: session[:user_id].to_i)
+  if @user.save
+    session[:user_id] = @users.id
+    redirect '/' # need to redirect to next page
+  else
+    erb :'/' 
+  end
+end
+
+# Creates a new user and adds to database
+# Reloads the page, need to implement redirect
