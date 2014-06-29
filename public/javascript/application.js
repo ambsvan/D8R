637497755dc93cd47bac6cfb88628ab4c576a3f7
ambsvan <amberslouise@gmail.com>
2014-06-29 22:38:39 +0000

    big css overhaul & main page gender form updated to dropdown

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,3 +2,7 @@ $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+$('a.nice-link').each(function () {
+  $(this).attr('data-text', $(this).text());
+});
